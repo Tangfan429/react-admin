37c9c773ca9c1d0043e13933a05ab9a32da02f8e
tangfan <75621980@qq.com>
2024-09-24 14:20:54 +0800

    feat: 新增test less文件

--- src/pages/testComponents/index.tsx
+++ src/pages/testComponents/index.tsx
@@ -1,5 +1,6 @@
-import React from 'react'
+import React from "react";
+import "./test.less";
 
 export default function TestComponents() {
-  return <div>testComponents</div>
+  return <div>testComponents</div>;
 }
